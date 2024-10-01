3e24905d4aa87384da53f4ac9417f8b2835eb108
malachai <malachaimorrison@gmail.com>
2024-10-01 15:21:24 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const name = "Thomas";
+const name1 = "Ahmet";
+const name2 = "Paul";
+const name3 = "Sam";
+
+import chalk from "chalk";
+
+console.log(chalk.blue(name));
+console.log(chalk.green(name1));
+console.log(chalk.red(name2));
+console.log(chalk.white(name3));
+
+/*const userInfromation = require("./userinformation");
+console.log(
+	`Je vous present ${userInfromation.name} ${userInfromation.name1} ${userInfromation.name2} ${userInfromation.name3}`,
+);*/
---
+++ userinformation.js
@@ -0,0 +1,11 @@
+const name = "Thomas";
+const name1 = "Ahmet";
+const name2 = "Paul";
+const name3 = "Sam";
+
+/*module.exports = {
+	name,
+	name1,
+	name2,
+	name3,
+};*/
